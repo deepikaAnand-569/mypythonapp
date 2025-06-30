83cf9b72aa2ce427f5c102b7e5f1b72a842659b7
deepikaAnand-569 <sonusunny569@gmail.com>
2025-06-30 15:14:38 +0530

    sec commit

---
+++ Variables.py
@@ -0,0 +1,17 @@
+# # age=18
+# if age>18:
+#     print("Eligible")
+# elif age==18:
+#     print("Welcome new voter")
+# else:
+#     print("not eligible")
+#     print("if condition execution")
+#     a=
+list=[]
+for i in range(n);
+    if i%2==0:
+        list1.append(i)
+        print(list1)
+        #comprehensive list => used dealing with huge data
+        list2=[x for x in range(n) if x%2==1]
+        print(list2)
